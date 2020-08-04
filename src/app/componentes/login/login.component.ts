2c26e72c0f333a2390f3319b54fae35f70d313b0
Andres Felipe <andres.garcia.gaviria@correounivalle.edu.co>
2020-08-04 16:53:15 -0500

    Validacion Login Contraseña

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -18,16 +18,18 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 export class LoginComponent implements OnInit {
 
+  // Expresión regular para validacion del email
   private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  LoginUsuario = {
-    CorreoElectronico: '',
-    Contrasena: ''
-  };
+  // LoginUsuario = {
+  //   CorreoElectronico: '',
+  //   Contrasena: ''
+  // };
 
+  // Formulario Login
   loginForm = new FormGroup({
     CorreoElectronico: new FormControl('', [Validators.required, Validators.pattern(this.emailPattern)]),
-    Contraseña: new FormControl('', [Validators.required, Validators.maxLength(8)])
+    Contrasena: new FormControl('', [Validators.required])
   });
 
   constructor(private loginService: LoginService) { }
@@ -40,16 +42,40 @@ export class LoginComponent implements OnInit {
 
   }
 
+  // Validacion del campo Correo Electronico
   validarCorreoElectronico() {
+    const CorreoElectronico = document.getElementById('CorreoElectronico');
+    const errorCorreoElectronico = document.getElementById('erroresCorreoElectronico');
+    if (this.CorreoElectronico.errors != null) {
+      if (this.CorreoElectronico.errors.required) {
+        CorreoElectronico.className = 'form-control is-invalid';
+        errorCorreoElectronico.innerHTML = 'Este campo es requerido';
+      }else if (this.CorreoElectronico.errors.pattern) {
+        CorreoElectronico.className = 'form-control is-invalid';
+        errorCorreoElectronico.innerHTML = 'Este campo debe ser de tipo email';
+      }
+    }else {
+      CorreoElectronico.className = 'form-control is-valid';
+    }
+  }
 
+  // Validacion del campo contraseña
+  validarContrasena() {
+    const Contrasena = document.getElementById('Contrasena');
+    const errorContrasena = document.getElementById('erroresContrasena');
+    if (this.Contrasena.errors != null) {
+      if (this.Contrasena.errors.required) {
+        Contrasena.className = 'form-control is-invalid';
+        errorContrasena.innerHTML = 'Este campo es requerido';
+      }
+    }else {
+      Contrasena.className = 'form-control is-valid';
+    }
   }
 
   Loguearse() {
     if (this.loginForm.valid) {
-      // this.loginService.PostLoguearse({
-      //   CorreoElectronico: this.LoginUsuario.CorreoElectronico,
-      //   Contrasena: this.LoginUsuario.Contrasena
-      // });
+      // this.loginService.PostLoguearse(this.loginForm.value)
       // .subscribe(
         // res => {
         //   alert(res.Estado);
@@ -64,6 +90,7 @@ export class LoginComponent implements OnInit {
     }
   }
 
+  // Valores del formulario login
   get CorreoElectronico() { return this.loginForm.get('CorreoElectronico'); }
-  get Contraseña() { return this.loginForm.get('Contraseña'); }
+  get Contrasena() { return this.loginForm.get('Contrasena'); }
 }
